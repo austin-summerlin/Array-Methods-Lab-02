eec92ed421e277b203b031bf0cd73d0b9a27034b
Austin Summerlin <summerlin.austin@gmail.com>
2021-06-08 17:31:04 -0700

    filter Red test

--- array-methods.js
+++ array-methods.js
@@ -3,3 +3,11 @@ export function map(arr, callback) {
   for (let i = 0; i < arr.length; i++) newArr[i] = callback(arr[i]);
   return newArr;
 }
+
+export function filter(arr, callback) {
+  const newArr = [];
+  for (const elem of arr)
+    if (callback(elem)) newArr[newArr.length] = elem;
+
+  return newArr;
+}
